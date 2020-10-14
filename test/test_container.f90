1afa7eb01b18ddd52ec2cfdde797691a043ca2a9
LKedward <laurence.kedward@bristol.ac.uk>
2020-10-14 11:19:54 +0100

    Refactor container type, remove built-in 1D support
    Adds getters for extracting intrinsic scalar values and pointers from the container type.

--- test/test_container.f90
+++ test/test_container.f90
@@ -17,9 +17,7 @@ module test_container
     
     testsuite = [ &
         & new_unittest("container-scalar", test_container_scalar), &
-        & new_unittest("container-scalar-ptr", test_container_scalar_ptr), &
-        & new_unittest("container-1d", test_container_1d), &
-        & new_unittest("container-1d-ptr", test_container_1d_ptr) &
+        & new_unittest("container-scalar-ptr", test_container_scalar_ptr) &
         ]
         
   end subroutine collect_container
@@ -89,78 +87,5 @@ module test_container
     end select
     
   end subroutine test_container_scalar_ptr
-
-
-  !> Set & update 1D data
-  subroutine test_container_1d(error)
-    type(error_t), allocatable, intent(out) :: error
-
-    integer(int32) :: my_data(6)
-    type(fhash_container_t) :: container
-
-    my_data = [0,1,2,3,4,5]
-
-    ! Set value
-    container = fhash_container(my_data)
-
-    if (.not.allocated(container%array1d_data)) then
-      call test_failed(error,'array1d_data not allocated.')
-      return
-    end if
-
-    select type(v=>container%array1d_data)
-    type is (integer(int32))
-      if (any(v /= my_data)) then
-        call test_failed(error,'Wrong value for container%array1d_data.')
-        return
-      end if
-    class default
-      call test_failed(error,'Wrong data type for container%array1d_data.')
-      return
-    end select
-
-  end subroutine test_container_1d
-
-  
-  !> Set & update 1D pointer
-  subroutine test_container_1d_ptr(error)
-    type(error_t), allocatable, intent(out) :: error
-
-    integer(int32), target :: my_data(6), new_data(2)
-    integer(int32), pointer :: check_ptr(:)
-    type(fhash_container_t) :: container
-    
-    my_data = [0,1,2,3,4,5]
-
-    ! Set value
-    container = fhash_container(my_data, pointer=.true.)
-
-    if (.not.associated(container%array1d_ptr)) then
-      call test_failed(error,'array1d_ptr not associated.')
-      return
-    end if
-
-    my_data = [5,4,3,2,1,0]
-
-    select type(v=>container%array1d_ptr)
-    type is (integer(int32))
-
-      check_ptr => v
-      if (.not.associated(v,my_data)) then
-        call test_failed(error,'Wrong association for array1d_ptr.')
-        return
-      end if
-
-      if (any(v /= my_data)) then
-        call test_failed(error,'Wrong value for container%array1d_ptr.')
-        return
-      end if
-
-    class default
-      call test_failed(error,'Wrong data type for container%array1d_ptr.')
-      return
-    end select
-
-  end subroutine test_container_1d_ptr
 
 end module test_container
